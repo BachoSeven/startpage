1d1723014365b00379a86500cc1e0d680466c3aa
Francesco Minnocci <ad17fmin@uwcad.it>
2020-12-22 16:22:09 +0100

    Get rid of useless `replaceChars` function: JavaScript has a [`.replace`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace) method!

--- js/search.js
+++ js/search.js
@@ -1,59 +1,41 @@
 // Implements a simple search bar with custom bangs
 
-// space to focus search
-document.addEventListener("keydown", event => {
-	if (event.code == 'Space') {
-		document.getElementById("q").focus();
-	}
-});
-
-String.prototype.replaceChars = function(character, replacement) {
-	var str = this;
-	var a;
-	var b;
-	for (var i=0; i < str.length; i++) {
-		if (str.charAt(i) == character) {
-			a = str.substr(0, i) + replacement;
-			b = str.substr(i + 1);
-			str = a + b;
-		}
-	}
-	return str;
-}
-
 function search(query) {
 	switch(query.substr(0, 1)){
 		case 'y':
 			query = query.substr(2);
 			window.open('https://www.youtube.com/results?search_query=' +
-			query.replaceChars(' ', '%20'), '_self');
+			query.replace(' ', '%20'), '_self'); // %20 is Space
 		break;
 		case 'w':
 			query = query.substr(2);
 			window.open(
 			'https://www.wikipedia.org/w/index.php?title=Special:Search&search=' +
-			query.replaceChars(' ', '%20'), '_self');
+			query.replace(' ', '%20'), '_self');
 		break;
 		case 'r':
 			query = query.substr(2);
 			window.open(
 			'https://www.reddit.com/search?q=' +
-			query.replaceChars(' ', '%20'), '_self');
+			query.replace(' ', '%20'), '_self');
 		break;
 		default:
 			window.open('https://duckduckgo.com/' +
-			query.replaceChars(' ', '%20'), '_self');
+			query.replace(' ', '%20'), '_self');
 	}
 }
 
 sinput = document.getElementById('q');
+// Typing (in Insert mode, when using Vimium) immediately focuses search bar
+document.addEventListener('keydown', event => {
+		sinput.focus();
+});
+// Enter accepts the search
 if (!!sinput) {
-	sinput.addEventListener('keypress', function(a) {
-	var key = a.keyCode;
-	if (key == 13) {
-		var query = this.value;
-		search(query);
-		sinput.value = '';
-	}
+	sinput.addEventListener('keydown', event => {
+		if (event.code == 'Enter') {
+			var query = sinput.value;
+			search(query);
+		}
 	});
 }
