71022d92f3a1fd5a097d4083eb2ae99e5dd5cd0b
Francesco Minnocci <ad17fmin@uwcad.it>
2020-12-22 14:33:26 +0100

    search.js:
    Add `replaceChars` and `search` functions to implement a barebones, "vanilla js" 'bangs' system for the search bar, instead of using the `form` html tag.
    
    We use the `window.open()` method with `_self` attribute to open urls in
    the current tab (meaning, they replace the startpage).

--- js/search.js
+++ js/search.js
@@ -6,3 +6,54 @@ document.addEventListener("keydown", event => {
 		document.getElementById("q").focus();
 	}
 });
+
+String.prototype.replaceChars = function(character, replacement) {
+	var str = this;
+	var a;
+	var b;
+	for (var i=0; i < str.length; i++) {
+		if (str.charAt(i) == character) {
+			a = str.substr(0, i) + replacement;
+			b = str.substr(i + 1);
+			str = a + b;
+		}
+	}
+	return str;
+}
+
+function search(query) {
+	switch(query.substr(0, 1)){
+		case 'y':
+			query = query.substr(2);
+			window.open('https://www.youtube.com/results?search_query=' +
+			query.replaceChars(' ', '%20'), '_self');
+		break;
+		case 'w':
+			query = query.substr(2);
+			window.open(
+			'https://www.wikipedia.org/w/index.php?title=Special:Search&search=' +
+			query.replaceChars(' ', '%20'), '_self');
+		break;
+		case 'r':
+			query = query.substr(2);
+			window.open(
+			'https://www.reddit.com/search?q=' +
+			query.replaceChars(' ', '%20'), '_self');
+		break;
+		default:
+			window.open('https://duckduckgo.com/' +
+			query.replaceChars(' ', '%20'), '_self');
+	}
+}
+
+sinput = document.getElementById('q');
+if (!!sinput) {
+	sinput.addEventListener('keypress', function(a) {
+	var key = a.keyCode;
+	if (key == 13) {
+		var query = this.value;
+		search(query);
+		sinput.value = '';
+	}
+	});
+}
