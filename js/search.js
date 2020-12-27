22b858164d2b1352df0fc3bc4f1abe4a4c39edfc
Francesco Minnocci <ad17fmin@uwcad.it>
2020-12-27 13:48:57 +0100

    Add `lichess` and `online-go` links; support `libgen` bang

--- js/search.js
+++ js/search.js
@@ -1,27 +1,34 @@
 // Implements a simple search bar with custom bangs
 
 function search(query) {
+	query = query.replace(/^[ ]/g,'')
 	switch(query.substr(0, 1)){
 		case 'y':
 			query = query.substr(2);
-			window.open('https://www.youtube.com/results?search_query=' +
-			query.replace(' ', '%20'), '_self'); // %20 is Space
+			window.open(
+				'https://www.youtube.com/results?search_query=' +
+				query.replace(' ', '%20'), '_self'); // %20 is Space
 		break;
 		case 'w':
 			query = query.substr(2);
 			window.open(
-			'https://www.wikipedia.org/w/index.php?title=Special:Search&search=' +
-			query.replace(' ', '%20'), '_self');
+				'https://www.wikipedia.org/w/index.php?title=Special:Search&search=' +
+				query.replace(' ', '%20'), '_self');
 		break;
+		case 'l':
+			query = query.substr(2);
+			window.open(
+				'http://93.174.95.27/search.php?req=' +
+				query.replace(' ', '%20'), '_self');
 		case 'r':
 			query = query.substr(2);
 			window.open(
-			'https://www.reddit.com/search?q=' +
-			query.replace(' ', '%20'), '_self');
+				'https://www.reddit.com/search?q=' +
+				query.replace(' ', '%20'), '_self');
 		break;
 		default:
 			window.open('https://duckduckgo.com/' +
-			query.replace(' ', '%20'), '_self');
+				query.replace(' ', '%20'), '_self');
 	}
 }
 
