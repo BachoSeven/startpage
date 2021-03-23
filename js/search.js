a553f9cfda9b03410fa0b13613c5262de28f5a8e
Francesco Minnocci <ad17fmin@uwcad.it>
2021-03-24 00:34:53 +0100

    Use the checkValidity method to enable direct input of URLs.

--- js/search.js
+++ js/search.js
@@ -1,68 +1,71 @@
 // Implements a simple search bar with custom bangs
 
-function search(query) {
-	query = query.replace(/^[ ]/g,'') // Remove leading spaces
-	if (query.substr(1, 1) == ' ') { // check if it's a bang (i.e. `y querywhichgoestoyoutube` and `a normal query` are parsed here, but `yquery` is not)
-		switch(query.substr(0, 1)){
+function search(q) {
+	var q = i.value;
+	q = q.replace(/^[ ]/g,'') // Remove leading spaces
+	if (i.checkValidity()) { // If query is an URL, go to it.
+		window.location=q;
+	} else if (q.substr(1, 1) == ' ') { // check if it's a bang (i.e. `y youtubequery` and `a query` are parsed here, but `query` is not)
+		switch(q.substr(0, 1)){
 			case 'y':
-				query = query.substr(2);
+				q = q.substr(2);
 				window.location=(
 					'https://www.youtube.com/results?search_query=' +
-					query.replace(' ', '%20')); // %20 is Space
+					q.replace(' ', '%20')); // %20 is Space
 			break;
 			case 'w':
-				query = query.substr(2);
+				q = q.substr(2);
 				window.location=(
 					'https://wikipedia.org/w/index.php?search=' +
-					query.replace(' ', '%20'));
+					q.replace(' ', '%20'));
 			break;
 			case 'g':
-				query = query.substr(2);
+				q = q.substr(2);
 				window.location=(
 					'https://github.com/search?q=' +
-					query.replace(' ', '%20'));
+					q.replace(' ', '%20'));
 			break;
 			case 'l':
-				query = query.substr(2);
+				q = q.substr(2);
 				window.location=(
 					'http://93.174.95.27/search.php?req=' +
-					query.replace(' ', '%20'));
+					q.replace(' ', '%20'));
 			break;
 			case 'r':
-				query = query.substr(2);
+				q = q.substr(2);
 				window.location=(
 					'https://www.reddit.com/search?q=' +
-					query.replace(' ', '%20'));
+					q.replace(' ', '%20'));
 			break;
 			case 'a':
-				query = query.substr(2);
+				q = q.substr(2);
 				window.location=(
 					'https://www.amazon.it/s/?field-keywords=' +
-					query.replace(' ', '%20'));
+					q.replace(' ', '%20'));
 			break;
 			default:
 				window.location=('https://duckduckgo.com/' +
-					query.replace(' ', '%20'));
+					q.replace(' ', '%20'));
 		}
-	} else { // this is were `normal query` will be parsed
+	} else { // this is were `normal q` will be parsed
 		window.location=('https://duckduckgo.com/' +
-			query.replace(' ', '%20'));
+			q.replace(' ', '%20'));
 	}
 }
 
-sinput = document.getElementById('q');
+i = document.getElementById('q');
 // Pressing space (in Insert mode) focuses search bar
 document.addEventListener('keydown', event => {
 	if (event.code == 'Space') {
-		sinput.focus();
+		i.focus();
 	}
 });
 // Enter accepts the search
-if (!!sinput) {
-	sinput.addEventListener('keydown', event => {
+if (!!i) {
+	i.addEventListener('keydown', event => {
 		if (event.code == 'Enter') {
-			var query = sinput.value;
-			search(query);
+			i.type = 'url';
+			search(i);
 		}
 	});
 }
