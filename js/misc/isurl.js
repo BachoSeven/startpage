7d81d33ea67e827c49320465e29496ce82c90f89
Francesco Minnocci <ad17fmin@uwcad.it>
2021-04-02 18:52:03 +0200

    Add js/misc folder (contains search bar utils)

---
+++ js/misc/isurl.js
@@ -0,0 +1,26 @@
+// First Method
+var inputElement = doc.createElement('input');
+inputElement.type = 'url'; // this is important
+// inputElement.value = url;
+if (!inputElement.checkValidity()) {
+    throw new TypeError('Invalid URL');
+}
+
+// Second Method
+function validURL(str) {
+	  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+		      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+		      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+		      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+		      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+		      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+	  return !!pattern.test(str);
+}
+
+// Third Method (Vimium)
+  // usage:
+  if (isUrl(q)) {
+      window.location=createFullUrl(q);
+  } else {
+  // (...)
+  }
